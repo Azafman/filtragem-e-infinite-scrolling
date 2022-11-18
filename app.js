f777da65a6ea05ae73fb8a6913f6f53519597276
Azafman <0000880867@senaimgaluno.com.br>
2022-11-18 11:05:42 -0300

    filterSearch00

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const divPostContainer = document.querySelector('#posts-container');
 const divLoader = document.querySelector('.loader');
+const inputValid = document.querySelector('#filter');
 let page = 1;
 let idPage = 0;
+
 
 const getPosts = async () => {
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=5&_page${page}`);
     console.log(page);
     return response.json();
 }
+const postTemplateInsertion = () => {
+
+}
 const addPostsInDOM = async () => {
     const data = await getPosts();
+
     const postTemplate = data.map(({title, body}) => {
         idPage++;
         return `<div class="post">
@@ -32,11 +38,29 @@ window.addEventListener('scroll', e => {
     // scrollHeight -> altura total do documento, incluindo as partes não visíveis  
     const {clientHeight, scrollHeight, scrollTop} = document.documentElement;
     const almostEndPage = scrollTop + clientHeight >= scrollHeight - 10;
-
+        
     //altura visível do documento mais a distância existente entre o topo do site com o topo da minha visualização são maiores que a altura total(visível e invisível) do documento - 10px ?
     if(almostEndPage) { 
         showLoading();
     }
+}) 
+window.addEventListener('input', e => {
+    //input-> toda vez que o valor de input for mudado
+    const valueOfSearch = e.target.value;
+    const valueOfFields = document.querySelectorAll('.post');
+
+    console.log(filterFields(valueOfSearch, valueOfFields)
+    .map(el => el.innerHTML)
+    .map(el => {
+        return el.split('')
+        .splice(0, 0, '<div class="post">')
+        .splice(el.length - 1, 0, "</div>")
+        .join('')
+    })
+   )
+    // divPostContainer.innerHTML = filterFields(valueOfSearch, valueOfFields)
+                                                                        // .map(el => el.innerHTML)
+                                                                        // .join('');
 })
 const showLoading = () => {
     divLoader.classList.add('show');
@@ -51,4 +75,17 @@ const removeShow = () => {
 const getNewPosts = () => {
     page++;
     addPostsInDOM();
+}
+const filterFields = (valorPesquisa = '', ValorCampos) => {
+    return Array.from(ValorCampos).filter( element => {
+           
+        const elementNumber = element.querySelector('.number');
+        const elementTitle = element.querySelector('.post-title');
+        const elementBody = element.querySelector('.post-body');
+
+        if (elementNumber.textContent.includes(valorPesquisa) || elementBody.textContent.includes(valorPesquisa) ||
+            elementTitle.textContent.includes(valorPesquisa)) {
+            return element;
+        }
+    })
 }
